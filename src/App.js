24e9deabd1c4d6040d0b79aaf420147ca8e8ef04
ayoub zaanouni <ayoubzaanouni9@gmail.com>
2023-01-01 13:08:13 +0100

    plain redux syntax and code for a basik store

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
+import Input from './Input';
+import Output from './Output';
+
 
 function App() {
   return (
     <div className="App">
-      ola
+      <Input />
+      <Output />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+
+
+// action // events 
+const increment = () => {
+  return {
+    type: "increment"
+  }
+}
+const decrement = () => {
+  return {
+    type: "decrement"
+  }
+}
+
+// reducer // like puting the finger on the triger before shoting, you need to know which finger i guess?????
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "increment": return state + 1;
+    case "decrement": return state - 1;
+  }
+}
+
+// store where we put our data
+
+let store = createStore(counter);
+
+// console 
+store.subscribe(() => { console.log(store.getState()) })
+
+// dispatch // fire the function
+store.dispatch(increment())
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
